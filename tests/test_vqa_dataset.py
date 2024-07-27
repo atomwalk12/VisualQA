9b501605a907ac637d70b38b7060a7d16bdcc8d3
Razvan <atomwalk12@gmail.com>
2024-07-27 22:55:37 +0200

    add additional test checks in test_get_image

--- tests/test_vqa_dataset.py
+++ tests/test_vqa_dataset.py
@@ -10,11 +10,13 @@ def initialized_dataset():
     wrapper.initialize()
     return wrapper
 
+
 def test_load_dataset(initialized_dataset):
     """
     Testing Dataset initialization
     """
     assert len(initialized_dataset.dataset) > 0
+
 
 def test_get_item(initialized_dataset):
     """
@@ -22,6 +24,15 @@ def test_get_item(initialized_dataset):
     """
     assert isinstance(initialized_dataset[0], EasyVQAElement)
 
-def test_dataset_fields(initialized_dataset):
+
+def test_get_image(initialized_dataset):
+    """
+    Testing retrieval of an image
+    """
     element: EasyVQAElement = initialized_dataset[0]
+
+    assert len(element.answer) > 0
+    assert len(element.question) > 0
+    assert len(element.image_path) > 0
+    assert element.image_id is not None
     assert isinstance(element.image, Image)
